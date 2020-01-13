a2efd181d4ae344994681e6d8182f8ec414a84eb
MUmarAmanat <umaramanat66@gmail.com>
2020-01-13 19:00:06 +0500

    Added auto_diff method

--- diff_n_inv_diff.py
+++ diff_n_inv_diff.py
@@ -1,8 +1,40 @@
 import numpy as np
+from statsmodels.tsa.stattools import adfuller
+
+def auto_difference(dataset):
+    """
+    Auto difference time series if doesnot want to enforce order of differencing
+    dataset:
+        type:list,
+        Desc: list contaning timeseries values
+    """
+    def do_diff(dataset):
+        diff = list()
+        for i in range(1, len(dataset)):
+            value = dataset[i] - dataset[i - 1]
+            diff.append(value)
+        return diff
+
+    diff_list = dataset
+    last_obs_value = []
+    not_stationary = True
+    
+    while not_stationary:
+        result = adfuller(dataset)
+        # if p-value is less than 0.005, its mean data is stationary and return 
+        if result[1] < 0.005:
+            not_stationary = False
+#             return diff_list, last_obs_value
+        else:
+            print(diff_list[0])
+            last_obs_value.append(diff_list[0])
+            diff_list = do_diff(diff_list)
+    return diff_list, last_obs_value
 
 
 def difference(dataset, interval=1):
     """
+    Differencing time series according to difference_order/interval
     dataset:
         type:list, 
         Desc: list contaning timeseries values
@@ -27,6 +59,7 @@ def difference(dataset, interval=1):
 
 def revert_difference(diff_forecast, last_obs_list = []):
     """
+    Reverting already difference data back to its original form
     diff_forecast:
         type:list
         Desc: Differenced forecast
